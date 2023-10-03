2e2e6e6ca380672df8e21dc2400bea00b3dbb00a
João_Lucas Silva <joaolucasacademico@gmail.com>
2023-10-03 17:35:58 -0300

    feat: Added more conditions
    For enabling the artillery button

--- ProjectFiles/buttonenabler.v
+++ ProjectFiles/buttonenabler.v
@@ -1,5 +1,6 @@
 module buttonenabler (
 	CH0, CH1,
+	CH7, CH6, CH5, CH4, CH3, CH2,
 	NCH0, NCH1,
 	IPT0, IPT1,
 	
@@ -8,6 +9,7 @@ module buttonenabler (
 
 	input
 	CH0, CH1,
+	CH7, CH6, CH5, CH4, CH3, CH2,
 	NCH0, NCH1,
 	IPT0, IPT1;
 	
@@ -15,12 +17,26 @@ module buttonenabler (
 	OUT0, OUT1;
 	
 	wire
+	NCH4, NCH3, NCH2,
+	isletteractive,
+	isnumberinactive0, isnumberinactive1, isnumberinactive2,
+	isnumberactive,
 	is0ON,
 	was0alwaysOFF;
 
-
 	
-and (is0ON, IPT0, CH0, CH1);
+not (NCH4, CH4);
+not (NCH3, CH3);
+not (NCH2, CH2);
+
+and (isnumberinactive0, NCH4, NCH3, NCH2);
+and (isnumberinactive1, CH4, CH3, NCH2);
+and (isnumberinactive2, CH4, CH3, CH2);
+
+or (isletteractive, CH7, CH6, CH5);
+nor (isnumberactive, isnumberinactive0, isnumberinactive1, isnumberinactive2);
+
+and (is0ON, IPT0, CH0, CH1, isletteractive, isnumberactive);
 
 srlt_gate gamestatus_0 (
 		.s (is0ON),
